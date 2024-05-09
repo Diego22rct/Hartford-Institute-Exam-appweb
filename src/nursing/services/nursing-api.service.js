ace18959c79fc94601fd36e2c2219b5f1c388f42
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-08 19:34:33 -0500

    feat: Add NursingApiService for nursing module

---
+++ src/nursing/services/nursing-api.service.js
@@ -0,0 +1,10 @@
+import GeneralApiService from "../../public/services/general-api.service.js";
+
+export default class NursingApiService extends GeneralApiService {
+    /**
+     * Creates an instance of NursingApiService.
+     */
+    constructor() {
+        super();
+    }
+}
