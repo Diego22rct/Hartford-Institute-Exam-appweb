a557de7e52077fe02dfd6c0685516148798f6aa6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2024-05-08 20:31:58 -0500

    feat: Update examiner card component with translated content

--- src/nursing/components/examiner-card.component.vue
+++ src/nursing/components/examiner-card.component.vue
@@ -1,33 +1,38 @@
 <script>
-
 export default {
-  name: 'ExaminerCard',
-  props: {
-    examiner: {
-      type: Object,
-      required: true
-    }
-  }
-}
+	name: "ExaminerCard",
+	props: {
+		examiner: {
+			type: Object,
+			required: true,
+		},
+	},
+};
 </script>
 
 <template>
-  <div>
-  <pv-card>
-    <template #title>
-      <h3>{{ examiner.fullName }} NPI:{{examiner.nationalProviderIdentifier}}</h3>
-    </template>
-    <template #subtitle>
-      <h4>{{$t("nursing.card.title")}}</h4>
-    </template>
-    <template #content>
-      <p>{{ $t("nursing.card.CurrentExamCount")}}: <b>{{examiner.totalExams}}</b></p>
-      <p>{{$t("nursing.card.AverageExam")}}: <b>{{examiner.averageScore}}</b></p>
-    </template>
-  </pv-card>
-  </div>
+	<div>
+		<pv-card>
+			<template #title>
+				<h3>
+					{{ examiner.fullName }} NPI:{{ examiner.nationalProviderIdentifier }}
+				</h3>
+			</template>
+			<template #subtitle>
+				<h4>{{ $t("nursing.card.title") }}</h4>
+			</template>
+			<template #content>
+				<p>
+					{{ $t("nursing.card.CurrentExamCount") }}:
+					<b>{{ examiner.totalExams }}</b>
+				</p>
+				<p>
+					{{ $t("nursing.card.AverageExam") }}:
+					<b>{{ examiner.averageScore }}</b>
+				</p>
+			</template>
+		</pv-card>
+	</div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
